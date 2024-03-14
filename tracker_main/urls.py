605b332f8f264c91ada2fe464498d7c58573dedc
AlaminSarkerFRII <alamin.education1997@gmail.com>
2024-03-15 00:53:59 +0600

    all api for company list

--- tracker_main/urls.py
+++ tracker_main/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import CompanyListAPI, EmployeeListAPI, DeviceListAPI , DeviceLogListAPI
+from .views import CompanyListAPI,CompanyDeleteAPI, EmployeeListAPI, DeviceListAPI , DeviceLogListAPI
+
 
 urlpatterns = [
         path('company-list/', CompanyListAPI.as_view()),
+        path('company/create/', CompanyListAPI.as_view()),
+        path('company/update/<int:id>', CompanyListAPI.as_view()),
+        path('company/delete/<int:id>/', CompanyDeleteAPI.as_view(), name='company-delete'),
+        
         path('employee-list/', EmployeeListAPI.as_view()),
         path('device-list/', DeviceListAPI.as_view()),
         path('device-log-list/', DeviceLogListAPI.as_view()),
+        
+        path('employees/<int:pk>/', EmployeeListAPI.as_view(), name='employee-detail'),
+        path('devices/<int:pk>/', DeviceListAPI.as_view(), name='device-detail'),
+        path('device-logs/<int:pk>/', DeviceLogListAPI.as_view(), name='device-log-detail'),
+        
 ]
--- tracker_main/views.py
+++ tracker_main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST,HTTP_404_NOT_FOUND
 from rest_framework.views import APIView
+from rest_framework.permissions import DjangoModelPermissions
 
 from .models import Company, Device, Employee, DeviceLog
 from .serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer, DeviceLogSerializer
@@ -32,6 +33,84 @@ class CompanyListAPI(APIView):
             result['data'] = companies_serializer
             return Response(result)
         return Response(result_error)
+    
+    
+    def post(self, request):
+        result = {
+                'status':  HTTP_200_OK,
+                'message': 'Successfully created a company',
+                'data':    []
+        }
+
+        result_error = {
+                'status':  HTTP_400_BAD_REQUEST,
+                'message': 'Bad Request or Invalid Request',
+                'data':    []
+        }
+        
+        serializer = CompanySerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            result['data'] = serializer.data
+            return Response(result)
+        
+        result_error['data'] = serializer.errors
+        
+        return Response(result_error)
+    
+    
+    
+    def put(self, request, id):
+        result = {
+                'status':  HTTP_200_OK,
+                'message': 'Successfully company updated',
+                'data':    []
+        }
+
+        result_error = {
+                'status':  HTTP_400_BAD_REQUEST,
+                'message': 'Company Not Found',
+                'data':    []
+        }
+        
+        company = Company.objects.get(id=id)
+        
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            result['data'] = serializer.data
+            return Response(result)
+        
+        result_error['data'] = serializer.errors
+        return Response(result_error)
+    
+
+    
+class CompanyDeleteAPI(APIView):
+    permission_classes = []
+
+    def get_queryset(self):
+        return Company.objects.all()
+
+    def delete(self, request, id):
+        result = {
+            'status': HTTP_200_OK,
+            'message': 'Success',
+        }
+
+        company = self.get_queryset().filter(id=id).first()
+        if company:
+            company.delete()
+            result['message'] = "Successfully Deleted Company"
+        else:
+            result['status'] = HTTP_404_NOT_FOUND
+            result['message'] = "Company Not Found"
+
+        return Response(result)
+
+    
+    
 
 
 # ================= > Employee List API view ================= >
